644088ef9326ee2e68d62b9e4a9fdf6215236591
Dis-count <492193947@qq.com>
2019-11-10 22:57:49 +0800

    更新手中Code

---
+++ 《Python编程》源代码文件/chapter_04/magicians.py
@@ -0,0 +1,15 @@
+magicians = ['alice', 'david', 'carolina'] 
+for magician in magicians: 
+    print(magician.title() + ", that was a great trick!")  
+    print("I can't wait to see your next trick, " + magician.title() + ".\n") 
+    
+print("Thank you everyone, that was a great magic show!")
+
+
+#  This expression
+# $\sqrt{3x-1}+(1+x)^2$ is an example of a $\LaTeX$ inline equation.he Lorenz 
+# Equations:
+# $$\begin{aligned}\dot{x} & = \sigma(y-x) \\dot{y} & = \rho x - y - xz \\dot{z} & = -\beta z + xy\end{aligned}$$
+
+
+
